ec4fb6018e6fdc08c6b400f592d3b8cafbf0a316
aledc7 <alejandrocdecastro@gmail.com>
2022-02-27 18:44:43 -0300

    Footer ready, and add images

---
+++ src/components/Footer.jsx
@@ -0,0 +1,125 @@
+import styled from 'styled-components';
+
+
+import {
+    Facebook, 
+    Instagram, 
+    Pinterest,
+    Twitter,
+    Room,
+    Phone,
+    MailOutline,
+} from '@mui/icons-material';
+
+const Container = styled.div`
+    display:flex;
+`;
+
+const Logo = styled.h1` 
+
+`;
+const Description = styled.p` 
+    margin: 20px 0px;
+`;
+const SocialContainer = styled.div` 
+    display:flex;
+`;
+const SocialIcon = styled.div` 
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color:white;
+    background-color: ${props=>props.color};
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
+`;
+
+const Left = styled.div`
+flex:1;
+display:flex;
+flex-direction:column;
+padding: 20px;
+`;
+const Center = styled.div`
+flex:1;
+padding: 20px;
+`;
+const Right = styled.div`
+flex:1;
+padding: 20px;
+`;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`;
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display:flex;
+    flex-wrap:wrap;
+`;
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Paiment = styled.img`
+    width: 50%;
+`;
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>Ale DC</Logo>
+                <Description>Lorem Ipsum</Description>
+                <SocialContainer>
+                    <SocialIcon color="#3B5999"> 
+                        <Facebook/>
+                    </SocialIcon>
+                    <SocialIcon color="#E4405F"> 
+                        <Instagram/>
+                    </SocialIcon>
+                    <SocialIcon color="#55ACEE"> 
+                        <Twitter/>
+                    </SocialIcon>
+                    <SocialIcon color="#E60023"> 
+                        <Pinterest/>
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List> 
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem> <Room style={{marginRight:"10px"}} /> Rioja 2776, Rosario, Argentina - CP: (2000) </ContactItem>
+                <ContactItem> <Phone style={{marginRight:"10px"}}/> +54 3415498622</ContactItem>
+                <ContactItem> <MailOutline style={{marginRight:"10px"}}/> ing.aledc@gmail.com</ContactItem>
+                <Paiment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+
+        </Container>
+    )
+}
+
+export default Footer
