3f68dab12b339231bf8e0e1fd4e3c35331528f35
aledc7 <alejandrocdecastro@gmail.com>
2022-02-28 14:39:08 -0300

    register page and login page in 80% finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Product from "./pages/Product/Product";
 import Home from "./pages/Home/Home";
 import ProductList from "./ProductList/ProductList";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 const App = () => {
-  return <Product/>
+  return <Login/>
 };
 
 export default App;
