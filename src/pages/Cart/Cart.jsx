e5bac898e5ee119ad6a9e0ee1b47824cc8f56da4
aledc7 <alejandrocdecastro@gmail.com>
2022-02-28 17:47:55 -0300

    cart is completed

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -101,6 +101,10 @@ const PriceDetail = styled.div`
 
 const Summary = styled.div`
     flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
 `;
 
 const ProductAmountContainer = styled.div`
@@ -123,6 +127,37 @@ const Hr = styled.hr`
     background-color: #eee;
     border: none;
     height: 1px;
+`;
+
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props=>props.type === "total" && "500"};
+    font-size: ${props=>props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span`
+
+`;
+
+const SummaryItemPrice = styled.span`
+
+`;
+
+const SummaryButton = styled.button`
+
+`;
+const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
 `;
 
 const Cart = () => {
@@ -182,7 +217,26 @@ const Cart = () => {
                             </PriceDetail>
                         </Product>
                     </Info>
-                    <Summary>summary</Summary>
+                    <Summary>
+                        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                        <SummaryItem>
+                            <SummaryItemText>Subtotal</SummaryItemText>
+                            <SummaryItemPrice>$ 449,99</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem>
+                            <SummaryItemText>Estimated Shipping</SummaryItemText>
+                            <SummaryItemPrice>$ 30,99</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem>
+                            <SummaryItemText>Shipping Discount</SummaryItemText>
+                            <SummaryItemPrice>$ 30,99</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem type='total'>
+                            <SummaryItemText>Total</SummaryItemText>
+                            <SummaryItemPrice>$ 449,99</SummaryItemPrice>
+                        </SummaryItem>
+                        <Button>CHECKOUT NOW</Button>
+                    </Summary>
                 </Bottom>
                 <Footer />
             </Wrapper>
